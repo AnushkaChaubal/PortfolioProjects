693b0b7cfe99bb5946c65fd7fdc9b2aceb479411
Anushka Chaubal <61931152+AnushkaChaubal@users.noreply.github.com>
2023-02-18 01:26:21 -0600

    Update Covid_Data_Exploration.sql

--- Covid_Data_Exploration.sql
+++ Covid_Data_Exploration.sql
@@ -82,15 +82,14 @@ on d.location=v.location and d.date=v.date;
 
 --Getting total vaccinations done for each location grouped by date
 with cte as
-(Select dea.continent, dea.location, dea.date, dea.population
-, MAX(vac.total_vaccinations) as RollingPeopleVaccinated
---, (RollingPeopleVaccinated/population)*100
-From public.coviddeaths dea
-Join public.covidvacc vac
-	On dea.location = vac.location
-	and dea.date = vac.date
-where dea.continent is not null 
-group by dea.continent, dea.location, dea.date, dea.population
-order by 1,2,3)
+(Select d.continent, d.location, d.date, d.population
+, max(vac.total_vaccinations) as RollingPeopleVaccinated
+from public.coviddeaths d
+join public.covidvacc v
+	on d.location = v.location
+	and d.date = v.date
+where d.continent is not null 
+group by d.continent, d.location, d.date, d.population
+order by  d.continent, d.location, d.date )
 select * from cte
 where RollingPeopleVaccinated is not null
